a9590f45ab6e2b32dfb4d39882c73a4b7f0d99c2
Cameron Beeler <cameron.beeler@icloud.com>
2023-06-10 22:49:23 -0700

    remove terraform files

--- lab2/main.tf
+++ lab2/main.tf
@@ -14,14 +14,30 @@ provider "aws" {
     profile = "terraform"
 }
 
+# Terraform Data Block - Lookup Ubuntu 20.04
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
 resource "aws_instance" "cam-one" {
-    ami = "<AMI>"
+    ami = "data.aws_ami.ubuntu.id"
     instance_type = "t3.micro"
 
-    subnet_id = "<SUBNET>"
-    vpc_security_group_ids = ["<SECURITY_GROUP>"]
+#   subnet_id = "<SUBNET>"
+#   vpc_security_group_ids = ["<SECURITY_GROUP>"]
 
     tags = {
-        "Identity" = "<IDENTITY>"
+        "Identity" = "Cams_default_VPC_Ubuntu"
     }
 }
