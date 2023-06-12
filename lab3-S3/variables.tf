118b0a4dc63ee161eaf4101603df0a05a0ad90d6
Cameron Beeler <cameron.beeler@icloud.com>
2023-06-11 19:43:05 -0700

    updating lab3

--- lab3-S3/variables.tf
+++ lab3-S3/variables.tf
@@ -8,3 +8,19 @@ variable "aws_region" {
         error_message = "The aws_region value must be a valid region in the USA, starting with \"us-\"."
     }
 }
+
+variable "variables_sub_cidr" {
+    description = "CIDR Block for the Variables Subnet"
+    type        = string
+    default     = "10.0.250.0/24"
+}
+variable "variables_sub_az" {
+    description = "Availability Zone used Variables Subnet"
+    type        = string
+    default     = "us-west-2a"
+}
+variable "variables_sub_auto_ip" {
+    description = "Set Automatic IP Assigment for Variables Subnet"
+    type = bool 
+    default = true
+}
